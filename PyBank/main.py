a9499466e6a528c04c5108c77f1f96479173bfc9
Jessica Fett <jessfett@Jessicas-MacBook-Air.local>
2020-09-01 00:20:37 -0400

    HW3 Submission

---
+++ PyBank/main.py
@@ -0,0 +1,94 @@
+#import dependences
+import os
+import csv
+
+
+
+# csv file path
+budget_path = os.path.join('Resources', 'budget_data.csv')
+ 
+    
+    
+#establish variables needed
+monthcount = 0
+total = 0
+current_month = 0
+last_month = 0
+revenue_change = 0
+
+#create lists for changes
+revenue_list = []
+months = []    
+    
+    
+    
+#open file
+with open(budget_path) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+   
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+    
+
+
+    # gather monthly changes in revenue
+    for row in csvreader:
+        monthcount = monthcount + 1
+        months.append(row[0])
+        current_month = int(row[1])
+        total = total + current_month
+        if monthcount > 1:
+            revenue_change = current_month - last_month
+            revenue_list.append(revenue_change)
+        last_month = current_month
+
+        
+        
+#ccommands to find changes over months
+revenue_sum = sum(revenue_list)
+profit_average = revenue_sum / (monthcount-1)
+max_revenue_change = max(revenue_list)
+min_revenue_change = min(revenue_list)
+index_max = revenue_list.index(max_revenue_change)
+index_min = revenue_list.index(min_revenue_change)
+month_max = months[index_max+1]
+month_min = months[index_min+1]
+
+
+#create variables with clean outputs with commas and rounding
+rounded_profit_average=round(profit_average,2)
+comma_total='{:,}'.format(total)
+comma_max_month='{:,}'.format(max_revenue_change)
+comma_min_month='{:,}'.format(min_revenue_change)
+comma_average_change='{:,}'.format(rounded_profit_average)
+
+
+# print summary to console
+print("Financial Analysis")
+print("----------------------------------------")
+print(f"Total Months: {monthcount}")
+print(f"Total: ${comma_total}")
+print(f"Average Change: ${comma_average_change}")
+print(f"Greatest Increase in Profit: {month_max} (${comma_max_month})")
+print(f"Greatest Decrease in Profit: {month_min} (${comma_min_month})")
+
+
+
+#output summary
+output_summary=("Financial Analysis\n"
+"----------------------------------------\n"
+f"Total Months: {monthcount}\n"
+f"Total: ${comma_total}\n"
+f"Average Change: ${comma_average_change}\n"
+f"Greatest Increase in Profit: {month_max} (${comma_max_month})\n"
+f"Greatest Decrease in Profit: {month_min} (${comma_min_month})\n")
+
+
+
+#Write to Text File
+output_file="budget_data_output.txt"
+
+with open(output_file, "w") as txt_file:
+    txt_file.write(output_summary)
---
+++ PyPoll/main.py
@@ -0,0 +1,105 @@
+#import dependences
+import os
+import csv
+
+# csv file path
+election_path = os.path.join("Resources", "election_data.csv")
+
+#open file
+with open(election_path) as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=",")
+
+    print(csvreader)
+   
+    csv_header = next(csvreader)
+
+
+    
+#Create a list for unique candidates
+    candidate_name_unique = []
+
+#find candidate count
+    for row in csvreader:
+        candidate_name_unique.append(row[2])
+
+    candidate_count = [[row,candidate_name_unique.count(row)] for row in set(candidate_name_unique)]
+    
+
+    
+#create lists for unique names and votes
+    votes = []
+    name = []
+    
+#add unique names and votes to lists
+    for row in candidate_count:
+        name.append(row[0])
+        votes.append(row[1])
+
+    candidate_zip = zip(name, votes)
+    candidate_list = list(candidate_zip)
+
+    winner = max(votes)
+    
+#determine the winner name
+    for row in candidate_list:
+        if row[1] == winner:
+            winner_name = row[0]       
+            
+            
+#Find candidate percentages by dividing candidate totals by total votes
+votes = len(candidate_name_unique)
+
+total_correy = candidate_name_unique.count("Correy")
+percentage_correy = int(total_correy) / int(votes)
+
+total_otooley = candidate_name_unique.count("O'Tooley")
+percentage_tooley = total_otooley / votes
+
+total_li = candidate_name_unique.count("Li")
+percent_li = total_li / votes
+
+total_khan = candidate_name_unique.count("Khan")
+percentage_khan = total_khan / votes
+
+        
+#create variables with clean outputs with commas and rounding
+comma_total_khan='{:,}'.format(total_khan)
+comma_total_correy='{:,}'.format(total_correy)
+comma_total_li='{:,}'.format(total_li)
+comma_total_otooley='{:,}'.format(total_otooley)
+comma_total_votes='{:,}'.format(votes)
+    
+#print Election Summary and format to Percentage with 2 decimals                                           
+print("Election Results")
+print(f"-------------------------")
+print(f"Total Votes: {comma_total_votes}")
+print(f"-------------------------")
+print(f"Khan: {percentage_khan:.2%} ({comma_total_khan})")
+print(f"Correy: {percentage_correy:.2%} ({comma_total_correy})")
+print(f"Li: {percent_li:.2%} ({comma_total_li})")
+print(f"OTooley: {percentage_tooley:.2%} ({comma_total_otooley})")
+print(f"-------------------------")
+print(f"Winner: {winner_name}")
+print(f"-------------------------")
+
+
+#output summary
+output_summary=("Election Results\n"
+f"-------------------------\n"
+f"Total Votes: {comma_total_votes}\n"
+f"-------------------------\n"
+f"Khan: {percentage_khan:.2%} ({comma_total_khan})\n"
+f"Correy: {percentage_correy:.2%} ({comma_total_correy})\n"
+f"Li: {percent_li:.2%} ({comma_total_li})\n"
+f"OTooley: {percentage_tooley:.2%} ({comma_total_otooley})\n"
+f"-------------------------\n"
+f"Winner: {winner_name}\n"
+f"-------------------------\n")
+
+#Write to Text File
+output_file="election_data_output.txt"
+
+with open(output_file, "w") as txt_file:
+    txt_file.write(output_summary)
+
+
